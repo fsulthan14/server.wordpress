b359abd555439ee91c9941775ed4b8961458a28b
fsulthan14 <qihh.sulthan@gmail.com>
2025-03-22 20:44:46 +0700

    update crontab

--- cron/install.sh
+++ cron/install.sh
@@ -6,4 +6,19 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 dirName=$(dirName "${0}")
+PARENTDIR=${1}
+USERNAME=${2}
+BASE_TIME=$(hostname -I | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d'.' -f4)
+BACKUP_MINUTE=$(( BASE_TIME % 40 ))
+CLEAN_OLD=$(( 20 + BACKUP_MINUTE ))
 
+echo "[INFO] Installing Crontab for ${USERNAME}"
+
+crontab -l -u ${USERNAME} &>/dev/null && crontab -r -u ${USERNAME}
+
+sed -e "s@%BACKUP_MINUTE%@${BACKUP_MINUTE}@g" \
+    -e "s@%PARENTDIR%@${PARENTDIR}/${USERNAME}@g" \
+    -e "s@%CLEAN_OLD%@${CLEAN_OLD}@g" \
+    "${dirName}/cron.template" | crontab -u ${USERNAME}
+
+echo "[INFO] Done"
