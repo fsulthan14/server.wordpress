363755c003c26cf05b7acf9d0c32d799c3e7d9a6
fsulthan14 <qihh.sulthan@gmail.com>
2025-03-22 20:16:10 +0700

    add

--- script/install.sh
+++ script/install.sh
@@ -8,7 +8,7 @@ fi
 if [ "$#" -lt 3 ]; then
    echo "[ERROR] Wrong number of arguments"
    echo "Syntax is:"
-   echo "   ${0} <nxfilter-username> <nxfilter-dir> <nxfilter-version>"
+   echo "   ${0} <parent-dir> <username> <cloud-backup-folder>"
    exit 1
 fi
 
@@ -19,10 +19,25 @@ echo
 dirName=$(dirname "$0")
 PARENTDIR="${1}"
 USERNAME="${2}"
-DB_NAME=wpdb-${USERNAME}
+CLOUD_FOLDER="${3}"
+CLOUD_MAIL="${4}"
+CLOUD_PASS="${5}"
+DB_NAME=wp${USERNAME}
 BACKUP_NAME=${USERNAME}
+
+echo "[INFO] Installing Script "
+mkdir -p ${PARENTDIR}/${USERNAME}/bin
 
 sed -i -e "s@%USERNAME%@${BACKUP_NAME}@g" \
        -e "s@%PARENTDIR%@${PARENTDIR}/${USERNAME}@g" \
        -e "s@%DB_NAME%@${DB_NAME}@g" \
-       "${PARENTDIR}/${USERNAME}/wp-config.php"
+       -e "s@%CLOUD_FOLDER%@${CLOUD_FOLDER}@g" \
+       "${dirName}/bin/backupService"
+
+sed -i -e "s@%MEGA_MAIL%@${CLOUD_MAIL}@g" \
+       -e "s@%MEGA_PASS%@${CLOUD_PASS}@g" \
+       "${dirName}/bin/connect-mega.sh"
+
+cp ${dirName}/bin/* ${PARENTDIR}/${USERNAME}/bin
+chown -R ${USERNAME}.${USERNAME} ${PARENTDIR}/${USERNAME}/bin
+echo "[INFO] Done"
